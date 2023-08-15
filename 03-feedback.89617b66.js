839cc11644bf3bc7aa0433d2a1c798e278a00ab4
VarshavetsAleksandr <128036812+VarshavetsAleksandr@users.noreply.github.com>
2023-08-15 16:04:05 +0000

    Deploying to gh-pages from @ VarshavetsAleksandr/goit-js-hw-08@ea46f39c30b67b0f2867656ad57a3eed2a564fe1 🚀

--- 03-feedback.89617b66.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector(".feedback-form");console.dir(e.elements.message.value),console.dir(e)}();
-//# sourceMappingURL=03-feedback.89617b66.js.map
--- 03-feedback.b2148dfc.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector(".feedback-form");console.dir(e.elements.message.value),console.dir(e);
-//# sourceMappingURL=03-feedback.b2148dfc.js.map
